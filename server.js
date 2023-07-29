1bfe764f779ebf0fc8405000eaa055461dd6b86b
unknown <umernk42@gmail.com>
2023-07-29 10:14:42 +0500

    Added cors origins

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const app = express();
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['https://umernk42.github.io/WorkOuts_MERN/',' http://localhost:5173/WorkOuts_MERN/']
+}));
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
